d2a4710c98aa994b1755c1c82ee680cdc9d95c74
Eduardo Lamhut <elamhut@gmail.com>
2025-08-25 02:36:02 -0300

    Trying to add unreal support part1

--- init.lua
+++ init.lua
@@ -918,7 +918,8 @@ require('lazy').setup({
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
+  -- NOTE Unreal Engine Stuff!
+  { 'zadirion/Unreal.nvim', dependencies = { { 'tpope/vim-dispatch' } } },
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
